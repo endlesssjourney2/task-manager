77df266bb7defaa831e3dd5e3888255e3cb81dc3
endlesssjourney2 <endlessj297@gmail.com>
2026-05-03 00:53:03 +0200

    add tests for Project page

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,10 +1,45 @@
 import { useParams } from "react-router";
 import s from "./Project.module.css";
+import CustomHeader from "../../components/CustomHeader/CustomHeader";
+import { useState } from "react";
+import useTasks from "../../hooks/useTasks";
 
 const Project = () => {
-  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  return <div className={s.project}>Here is your project ID: {id}</div>;
+  const { id } = useParams();
+  const { tasks, addTask } = useTasks(id);
+
+  const handleAddTask = async () => {
+    await addTask(title, description, "medium", null);
+  };
+
+  return (
+    <div className={s.project}>
+      <CustomHeader title="Your tasks" />
+      <div className={s.content}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      {tasks.map((t) => (
+        <div className={s.test}>
+          <div>{t.title}</div>
+          <div>{t.description}</div>
+          <div>{t.due_date}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Project;
