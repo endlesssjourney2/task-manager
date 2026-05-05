0a64a8f5e77e0ca813ce671e1b30802914fadd90
endlesssjourney2 <endlessj297@gmail.com>
2026-05-05 21:18:14 +0200

    feat: integrate task creation modal to Project page

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -2,20 +2,31 @@ import { useParams } from "react-router";
 import s from "./Project.module.css";
 import CustomHeader from "../../components/CustomHeader/CustomHeader";
 import useTasks from "../../hooks/useTasks";
-import TasksInputs from "./components/TasksInputs/TasksInputs";
+import { useState } from "react";
+import TasksModal from "./components/TasksModal/TasksModal";
 import type { Priority } from "../../types/task";
 
 const Project = () => {
   const { id } = useParams();
-  const { tasks, addTask, removeTask } = useTasks(id);
+  const { tasks, addTask, removeTask, actionLoading } = useTasks(id);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   const handleAddTask = async (
     title: string,
     description: string,
     priority: Priority,
-    date: string,
+    date: string | null,
   ) => {
     const result = await addTask(title, description, priority, date);
+    if (result) setModalOpen(false);
     return result;
   };
 
@@ -23,16 +34,24 @@ const Project = () => {
     <div className={s.project}>
       <CustomHeader title="Your tasks" />
       <div className={s.content}>
-        <TasksInputs handleCreateTask={handleAddTask} />
+        <div className={s.addNew}>Add a new task for your project here</div>
+        <button onClick={handleOpenModal}>OPEN</button>
       </div>
       {tasks.map((t) => (
         <div className={s.test}>
           <div>{t.title}</div>
           <div>{t.description}</div>
           <div>{t.due_date}</div>
+          <div>{t.priority}</div>
           <button onClick={() => removeTask(t.id)}>Delete</button>
         </div>
       ))}
+      <TasksModal
+        modalOpen={modalOpen}
+        handleCloseModal={handleCloseModal}
+        handleCreateTask={handleAddTask}
+        loading={actionLoading}
+      />
     </div>
   );
 };
