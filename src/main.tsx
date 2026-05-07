3c6fe9af3d09d65bf5644004f11800493431093b
endlesssjourney2 <endlessj297@gmail.com>
2026-05-07 11:02:52 +0200

    style: add custom Ant Design dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,33 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
-
+import { ConfigProvider, theme } from "antd";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+          token: {
+            colorBgElevated: "#21262d",
+            colorBgContainer: "#0c1117",
+            colorBorder: "#3e444d",
+            colorText: "#f0f6fc",
+            colorTextPlaceholder: "#9298a1",
+            colorPrimary: "#0067f8ff",
+            borderRadius: 12,
+            padding: 15,
+          },
+          components: {
+            Modal: {
+              contentBg: "#21262d",
+              titleColor: "#f0f6fc",
+            },
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </AuthProvider>
   </StrictMode>,
 );
