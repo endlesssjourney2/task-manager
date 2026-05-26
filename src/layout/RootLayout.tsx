354cd12317e5dcdacd31e391642492de70256f88
endlesssjourney2 <endlessj297@gmail.com>
2026-05-27 01:19:37 +0200

    refactor: update RootLayout to use sidebar navigation

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,12 +1,22 @@
-import Navbar from "../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar/Sidebar";
+import { useState } from "react";
+import { Layout } from "antd";
+
+const { Content } = Layout;
 
 const RootLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <Layout>
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+      <Layout>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
