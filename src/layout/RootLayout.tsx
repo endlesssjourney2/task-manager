5b4c9f18ccaa1a7d7ed2bc3d21f8c0c41557d913
endlesssjourney2 <endlessj297@gmail.com>
2026-05-27 22:59:05 +0200

    refactor: connect Sidebar to projects context

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,16 +1,22 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar/Sidebar";
+import Sidebar from "../features/Sidebar/Sidebar";
 import { useState } from "react";
 import { Layout } from "antd";
+import { useProjectsContext } from "../context/ProjectsContext";
 
 const { Content } = Layout;
 
 const RootLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const { projects } = useProjectsContext();
 
   return (
     <Layout>
-      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+      <Sidebar
+        collapsed={collapsed}
+        setCollapsed={setCollapsed}
+        projects={projects}
+      />
       <Layout>
         <Content>
           <Outlet />
