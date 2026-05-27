d711afb87c63ed263f7897a9743c8c776373bf3b
endlesssjourney2 <endlessj297@gmail.com>
2026-05-27 22:55:24 +0200

    refactor: wrap app with ProjectsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import AppComponent from "./AppComponent.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { App, ConfigProvider, theme } from "antd";
+import { ProjectsProvider } from "./context/ProjectsContext.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
@@ -32,7 +33,9 @@ createRoot(document.getElementById("root")!).render(
         }}
       >
         <App>
-          <AppComponent />
+          <ProjectsProvider>
+            <AppComponent />
+          </ProjectsProvider>
         </App>
       </ConfigProvider>
     </AuthProvider>
