a39b17448d37c392b07f46f3fed4ef59332da233
endlesssjourney2 <endlessj297@gmail.com>
2026-05-29 00:32:09 +0200

    chore: add getRandomColor helper

---
+++ src/helpers/getRandomColor.ts
@@ -0,0 +1,9 @@
+export const getRandomColor = () => {
+  let color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+
+  while (color.length < 6) {
+    color = "0" + color;
+  }
+
+  return "#" + color.slice(0, 6);
+};
