74ee38b47bb8aef07e60ba5f152a1155ff9e86ab
endlesssjourney2 <endlessj297@gmail.com>
2026-05-30 23:17:31 +0200

    feat: add project edit modal

---
+++ src/features/Project/components/EditModalProject/EditModalProject.tsx
@@ -0,0 +1,62 @@
+import { ColorPicker, Input, Modal } from "antd";
+import s from "./EditModalProject.module.css";
+import { useState, type FC } from "react";
+import type { Project, UpdateProjectPayload } from "../../../../types/project";
+
+type Props = {
+  modalOpen: boolean;
+  handleCloseModal: () => void;
+  selectedProject: Project;
+  handleEditProject: (
+    id: string,
+    fields: Omit<UpdateProjectPayload, "id">,
+  ) => Promise<boolean>;
+  loading?: boolean;
+};
+
+const EditModalProject: FC<Props> = ({
+  modalOpen,
+  handleCloseModal,
+  selectedProject,
+  loading,
+  handleEditProject,
+}) => {
+  const [title, setTitle] = useState(selectedProject.title);
+  const [color, setColor] = useState(selectedProject.color);
+
+  const handleOk = async () => {
+    await handleEditProject(selectedProject.id, {
+      title,
+      color,
+    });
+  };
+
+  return (
+    <Modal
+      style={{ top: "250px" }}
+      closable={{ "aria-label": "Custom Close Button" }}
+      title={`Edit your project ${selectedProject.title}`}
+      open={modalOpen}
+      onCancel={handleCloseModal}
+      onOk={handleOk}
+      confirmLoading={loading}
+    >
+      <div className={s.modalContainer}>
+        <Input
+          onPressEnter={handleOk}
+          placeholder="Title..."
+          className={s.input}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <ColorPicker
+          className={s.datePicker}
+          value={color}
+          onChange={(e) => setColor(e.toHexString())}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default EditModalProject;
