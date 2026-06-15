a9c253729e1775c355dc6d162fd92cda3a21466e
endlesssjourney2 <endlessj297@gmail.com>
2026-06-15 16:38:47 +0200

    style: update AddModalProject layout

--- src/features/components/AddModalProject/AddModalProject.tsx
+++ src/features/components/AddModalProject/AddModalProject.tsx
@@ -1,4 +1,4 @@
-import { ColorPicker, Input, Modal } from "antd";
+import { Button, ColorPicker, Input, Modal } from "antd";
 import s from "./AddModalProject.module.css";
 import { useState, type FC } from "react";
 import { getRandomColor } from "../../../helpers/getRandomColor";
@@ -39,22 +39,38 @@ const HomeModal: FC<Props> = ({
       closable={{ "aria-label": "Custom Close Button" }}
       open={modalOpen}
       onCancel={handleCancel}
-      onOk={handleOk}
       style={{ top: "250px" }}
       confirmLoading={loading}
+      footer={[
+        <div className={s.footer}>
+          <Button type="default" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button type="primary" onClick={handleOk} loading={loading}>
+            Create Project
+          </Button>
+        </div>,
+      ]}
     >
       <div className={s.modalContent}>
         <Input
+          className={s.input}
           type="text"
           placeholder="Project title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           onPressEnter={handleOk}
-        />
-        <ColorPicker
-          defaultValue={color}
-          value={color}
-          onChange={(e) => setColor(e.toHexString())}
+          suffix={
+            <ColorPicker
+              value={color}
+              onChange={(e) => setColor(e.toHexString())}
+            >
+              <div
+                className={s.colorSwatch}
+                style={{ backgroundColor: color }}
+              />
+            </ColorPicker>
+          }
         />
       </div>
     </Modal>
