dfa3fbcd483fd494e9c491877992a2194e643d10
endlesssjourney2 <endlessj297@gmail.com>
2026-06-15 18:27:19 +0200

    refactor: add TasksProvider to app root

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { App, ConfigProvider, theme } from "antd";
 import { ProjectsProvider } from "./context/ProjectsContext.tsx";
 import { ProfileProvider } from "./context/ProfileContext.tsx";
+import { TasksProvider } from "./context/TasksContext.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
@@ -36,7 +37,9 @@ createRoot(document.getElementById("root")!).render(
         <App>
           <ProfileProvider>
             <ProjectsProvider>
-              <AppComponent />
+              <TasksProvider>
+                <AppComponent />
+              </TasksProvider>
             </ProjectsProvider>
           </ProfileProvider>
         </App>
