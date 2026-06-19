03c15b8e0992a1f5307be7135f05440e49fff134
endlesssjourney2 <endlessj297@gmail.com>
2026-06-19 19:27:14 +0200

    feat: redesign CustomStatus as dropdown circle

--- src/features/tasks/components/CustomStatus/CustomStatus.tsx
+++ src/features/tasks/components/CustomStatus/CustomStatus.tsx
@@ -1,53 +1,35 @@
 import { type FC } from "react";
-import s from "./CustomStatus.module.css";
 import { Dropdown, type MenuProps } from "antd";
-import { DownOutlined } from "@ant-design/icons";
 import type { Status } from "../../../../types/task";
-import { capitalizeFirst } from "../../../../helpers/capitalizeFirst";
+import s from "./CustomStatus.module.css";
 
 type Props = {
   status: Status;
-  id: string;
-  updateStatus: (id: string, status: Status) => Promise<boolean>;
+  onChange: (status: Status) => void;
 };
 
-const CustomStatus: FC<Props> = ({ status, updateStatus, id }) => {
+const CustomStatus: FC<Props> = ({ status, onChange }) => {
   const items: MenuProps["items"] = [
-    { key: "todo", label: "Todo" },
-    { key: "in_progress", label: "In progress" },
-    { key: "done", label: "Done" },
+    { key: "todo", label: "🔵 Todo" },
+    { key: "in_progress", label: "🟡 In progress" },
+    { key: "done", label: "🟢 Done" },
   ];
 
   return (
-    <div
-      className={`${s.status} 
-      ${
-        status === "todo"
-          ? s.todoBg
-          : status === "in_progress"
-            ? s.inProgressBg
-            : s.doneBg
-      }`}
+    <Dropdown
+      trigger={["click"]}
+      menu={{
+        items: items,
+        onClick: ({ key }) => {
+          onChange(key as Status);
+        },
+      }}
     >
-      <span
-        className={`${s.statusText} 
-        ${
-          status === "todo"
-            ? s.todo
-            : status === "in_progress"
-              ? s.inProgress
-              : s.done
-        }`}
-      >
-        {capitalizeFirst(status)}
-      </span>
-      <Dropdown
-        menu={{ items, onClick: ({ key }) => updateStatus(id, key as Status) }}
-        trigger={["click"]}
-      >
-        <DownOutlined />
-      </Dropdown>
-    </div>
+      <button
+        className={`${s.circle} ${s[status]}`}
+        onClick={(e) => e.stopPropagation()}
+      />
+    </Dropdown>
   );
 };
 
