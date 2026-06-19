1a4762fc0202307752b9e142b725795d9bc2fffd
endlesssjourney2 <endlessj297@gmail.com>
2026-06-19 19:28:57 +0200

    feat: update draft TasksList with status and actions

--- src/features/tasks/components/TasksList/TasksList.tsx
+++ src/features/tasks/components/TasksList/TasksList.tsx
@@ -1,73 +1,53 @@
 import type { FC } from "react";
 import s from "./TasksList.module.css";
 import type { Task } from "../../../../types/task";
-import { Typography } from "antd";
 import { useProjectTasksContext } from "../../../../context/ProjectTasksContext";
+import TaskDropdown from "../../../components/TaskDropdown/TaskDropdown";
+import CustomStatus from "../CustomStatus/CustomStatus";
+import { tomorrowDate } from "../../../../helpers/tomorrowDate";
 
 type Props = {
   tasks: Task[];
-  removeTask: (id: string) => void;
   handleOpenModal: (task: Task) => void;
 };
 
-const TasksList: FC<Props> = ({ tasks }) => {
+const TasksList: FC<Props> = ({ tasks, handleOpenModal }) => {
   const { editTask } = useProjectTasksContext();
-
-  const handleUpdateTitle = async (newTitle: string, id: string) => {
-    await editTask(id, { title: newTitle });
-  };
-
-  const handleUpdateDescription = async (newDesc: string, id: string) => {
-    await editTask(id, { description: newDesc });
-  };
 
   return (
     <>
       <ul className={s.list}>
         {tasks.map((t) => (
           <li className={s.item} key={t.id}>
-            <div className={s.left}>
-              <div className={s.itemInfo}>
-                <Typography.Title
-                  level={4}
-                  className={s.title}
-                  editable={{
-                    triggerType: ["text"],
-                    onChange: (newTitle) => {
-                      if (newTitle === t.title) return;
-                      handleUpdateTitle(newTitle, t.id);
-                    },
-                  }}
-                >
-                  {t.title}
-                </Typography.Title>
-                <Typography.Paragraph
-                  editable={{
-                    triggerType: ["text"],
-                    onChange: (newDesc) => {
-                      if (newDesc === t.description) return;
-                      handleUpdateDescription(newDesc, t.id);
-                    },
-                  }}
-                  className={s.subtitle}
-                >
-                  {t.description}
-                </Typography.Paragraph>
-              </div>
+            <div className={s.status}>
+              <CustomStatus
+                status={t.status}
+                onChange={(newStatus) => editTask(t.id, { status: newStatus })}
+              />
             </div>
-            <div className={s.right}>
-              <div className={s.date}>{t.due_date ?? "No date"}</div>
-              <div
-                className={s.priority}
-                style={{
-                  background:
-                    t.priority === "high"
-                      ? "#ef4444"
-                      : t.priority === "medium"
-                        ? "#f59e0b"
-                        : "#22c55e",
-                }}
-              ></div>
+            <div className={s.content}>
+              <div className={s.left}>
+                <div className={s.itemInfo}>
+                  <h2
+                    style={{
+                      textDecoration: t.status === "done" && "line-through",
+                    }}
+                    className={s.title}
+                  >
+                    {t.title}
+                  </h2>
+                  <span className={s.subtitle}>{t.description}</span>
+                  <div className={s.priority}>{t.priority}</div>
+                </div>
+              </div>
+              <div className={s.right}>
+                <div className={s.date}>{tomorrowDate(t.due_date)}</div>
+                <TaskDropdown
+                  id={t.id}
+                  task={t}
+                  handleOpenEditModal={handleOpenModal}
+                />
+              </div>
             </div>
           </li>
         ))}
