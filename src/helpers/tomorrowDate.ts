217c5427c43b90efd25f730f820acdc196e35815
endlesssjourney2 <endlessj297@gmail.com>
2026-06-22 20:54:34 +0200

    chore: add next Monday date helper

--- src/helpers/tomorrowDate.ts
+++ src/helpers/tomorrowDate.ts
@@ -1,4 +1,7 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+
+dayjs.extend(isoWeek);
 
 export const tomorrowDate = (date: string) => {
   if (!date) return "";
@@ -6,4 +9,9 @@ export const tomorrowDate = (date: string) => {
     dayjs(date).format("YYYY-MM-DD")
     ? "Tomorrow"
     : dayjs(date).format("D MMMM");
+};
+
+export const nextMonday = (date: Dayjs) => {
+  if (!date) return null;
+  return dayjs(date).add(1, "week").isoWeekday(1);
 };
