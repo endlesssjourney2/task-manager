2e2902d1db6516e8a8d7d82af04c3f9990fdfb36
endlesssjourney2 <endlessj297@gmail.com>
2026-06-25 21:44:10 +0200

    feat: add restore and remove handlers to useDoneTasks

--- src/hooks/useDoneTasks.ts
+++ src/hooks/useDoneTasks.ts
@@ -2,36 +2,61 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { getDoneTasks } from "../api/task";
 import useNotify from "./useNotify";
-import type { Task } from "../types/task";
+import type { TasksWithProjects } from "../types/task";
+import { useTasksContext } from "../context/TasksContext";
 
 const useDoneTasks = () => {
   const { user } = useAuth();
   const notify = useNotify();
-  const [doneTasks, setDoneTasks] = useState<Task[]>([]);
+  const { editTask, removeTask } = useTasksContext();
+  const [doneTasks, setDoneTasks] = useState<TasksWithProjects[]>([]);
   const [initialLoading, setInitialLoading] = useState(true);
+  const [actionLoading, setActionLoading] = useState(false);
+
+  const fetchDoneTasks = async () => {
+    const [data, error] = await getDoneTasks(user.id);
+    if (error) {
+      notify.notification.error(
+        "Failed to load tasks",
+        "Please try again or check console for more details",
+        "fetch-tasks-error",
+      );
+      console.error("Error fetching tasks:", error);
+      setInitialLoading(false);
+      return;
+    }
+    setDoneTasks(data as TasksWithProjects[]);
+    setInitialLoading(false);
+  };
 
   useEffect(() => {
     if (!user) return;
-    const fetchDoneTasks = async () => {
-      setInitialLoading(true);
-      const [data, error] = await getDoneTasks(user.id);
-      if (error) {
-        notify.notification.error(
-          "Failed to load tasks",
-          "Please try again or check console for more details",
-          "fetch-tasks-error",
-        );
-        console.error("Error fetching tasks:", error);
-        setInitialLoading(false);
-        return;
-      }
-      setDoneTasks(data as Task[]);
-      setInitialLoading(false);
-    };
+    setInitialLoading(true);
     fetchDoneTasks();
   }, [user]);
 
-  return { doneTasks, initialLoading };
+  const handleRestoreTask = async (taskId: string, newStatus: "todo") => {
+    setActionLoading(true);
+    await editTask(taskId, { status: newStatus });
+    setDoneTasks((prev) => prev.filter((task) => task.id !== taskId));
+    setActionLoading(false);
+  };
+
+  const handleRemoveTask = async (taskId: string) => {
+    setActionLoading(true);
+    await removeTask(taskId);
+    setDoneTasks((prev) => prev.filter((task) => task.id !== taskId));
+    setActionLoading(false);
+  };
+
+  return {
+    doneTasks,
+    initialLoading,
+    refetch: fetchDoneTasks,
+    handleRestoreTask,
+    actionLoading,
+    handleRemoveTask,
+  };
 };
 
 export default useDoneTasks;
