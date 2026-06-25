a6fe200d87616482436de9afe0e2512752478683
endlesssjourney2 <endlessj297@gmail.com>
2026-06-25 21:44:39 +0200

    feat: expose edit and remove actions in TasksContext

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -4,15 +4,19 @@ import useTasks from "../hooks/useTasks";
 type TasksContextType = {
   addTask: ReturnType<typeof useTasks>["addTask"];
   actionLoading: ReturnType<typeof useTasks>["actionLoading"];
+  editTask: ReturnType<typeof useTasks>["editTask"];
+  removeTask: ReturnType<typeof useTasks>["removeTask"];
 };
 
 const TasksContext = createContext<TasksContextType | null>(null);
 
 export const TasksProvider = ({ children }: { children: ReactNode }) => {
-  const { addTask, actionLoading } = useTasks();
+  const { addTask, actionLoading, editTask, removeTask } = useTasks();
 
   return (
-    <TasksContext.Provider value={{ actionLoading, addTask }}>
+    <TasksContext.Provider
+      value={{ actionLoading, addTask, editTask, removeTask }}
+    >
       {children}
     </TasksContext.Provider>
   );
