75d1327af51a4ebdc78484d7bf93fb0b1ce59eb5
endlesssjourney2 <endlessj297@gmail.com>
2026-06-26 20:11:01 +0200

    refactor: rename date helper and add relative date formatter

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -1,9 +1,11 @@
 import dayjs, { Dayjs } from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
+import relativeTime from "dayjs/plugin/relativeTime";
 
+dayjs.extend(relativeTime);
 dayjs.extend(isoWeek);
 
-export const tomorrowDate = (date: string) => {
+export const formatDueDate = (date: string) => {
   if (!date) return "No date";
   return dayjs().add(1, "day").format("YYYY-MM-DD") ===
     dayjs(date).format("YYYY-MM-DD")
@@ -14,4 +16,9 @@ export const tomorrowDate = (date: string) => {
 export const nextMonday = (date: Dayjs) => {
   if (!date) return null;
   return dayjs(date).add(1, "week").isoWeekday(1);
+};
+
+export const relativeDate = (date: string) => {
+  if (!date) return null;
+  return dayjs(date).fromNow();
 };
