bc649e29d72ef51be7e402b29c18f9d8fbfbd35f
endlesssjourney2 <endlessj297@gmail.com>
2026-07-07 19:39:51 +0200

    feat: add task completion and reschedule actions

--- src/pages/Today/Today.tsx
+++ src/pages/Today/Today.tsx
@@ -1,9 +1,15 @@
 import s from "./Today.module.css";
 import useTodayTasks from "../../hooks/useTodayTasks";
 import CustomHeader from "../../components/CustomHeader/CustomHeader";
+import { Checkbox, Tooltip } from "antd";
 
 const Today = () => {
-  const { todayTasks } = useTodayTasks();
+  const {
+    todayTasks,
+    handleDoneTask,
+    handleTomorrowUpdate,
+    handleNextWeekUpdate,
+  } = useTodayTasks();
 
   return (
     <div className={s.today}>
@@ -13,6 +19,10 @@ const Today = () => {
         {todayTasks.map((t) => (
           <li key={t.id} className={s.item}>
             <div className={s.content}>
+              <Tooltip title="Mark as done" color={"green"} placement="left">
+                <Checkbox onChange={() => handleDoneTask(t.id)} />
+              </Tooltip>
+
               <div className={s.left}>
                 <h2 className={s.title}>{t.title}</h2>
                 {t.description && (
@@ -31,9 +41,21 @@ const Today = () => {
                     # {t.projects.title}
                   </span>
                 </div>
-                {/* <div className={s.buttons}>
-                  <button>Edit date</button> work in progress
-                </div> */}
+                <div className={s.buttons}>
+                  <button
+                    className={`${s.button}`}
+                    onClick={() => handleTomorrowUpdate(t.id)}
+                  >
+                    Tomorrow
+                  </button>
+
+                  <button
+                    className={`${s.button}`}
+                    onClick={() => handleNextWeekUpdate(t.id)}
+                  >
+                    Next week
+                  </button>
+                </div>
               </div>
             </div>
           </li>
