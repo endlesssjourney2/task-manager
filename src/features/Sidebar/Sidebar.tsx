71ef0f0fd45a533eddce429ec566875f33e030a7
endlesssjourney2 <endlessj297@gmail.com>
2026-08-01 00:03:23 +0200

    feat: add Overdue tasks navigation link

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -163,21 +163,28 @@ const Sidebar: FC<Props> = ({ collapsed, setCollapsed }) => {
                 </div>
               )}
               <Divider />
+              <div className={s.links}>
+                <div
+                  onClick={() => navigate("/app/today")}
+                  className={s.sidebarLink}
+                >
+                  <span className={s.title}>Today's tasks</span>
+                  <IconCalendarClock stroke={1.3} />
+                </div>
 
-              <div
-                onClick={() => navigate("/app/today")}
-                className={s.todayTasks}
-              >
-                <span className={s.title}>Today's tasks</span>
-                <IconCalendarClock stroke={1.3} />
-              </div>
-
-              <div
-                onClick={() => navigate("/app/done")}
-                className={s.doneTasks}
-              >
-                <span className={s.title}>Done tasks </span>
-                <IconCircleCheck stroke={1.3} />
+                <div
+                  onClick={() => navigate("/app/done")}
+                  className={s.sidebarLink}
+                >
+                  <span className={s.title}>Done tasks </span>
+                  <IconCircleCheck stroke={1.3} />
+                </div>
+                <div
+                  onClick={() => navigate("/app/overdue")}
+                  className={s.sidebarLink}
+                >
+                  <span className={s.title}>Overdue tasks</span>
+                </div>
               </div>
             </>
           )}
