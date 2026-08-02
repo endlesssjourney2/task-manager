d1b567b032d85ad88978fd990fb261913c24c4f3
endlesssjourney2 <endlessj297@gmail.com>
2026-08-02 23:35:22 +0200

    chore: add overdue severity colors

---
+++ src/constants/overdue.ts
@@ -0,0 +1,11 @@
+export const OVERDUE_COLORS = {
+  recent: "#f59e0b",
+  week: "#f97316",
+  old: "#dc2626",
+};
+
+export const OVERDUE_BG = {
+  recent: "#f59e0b14",
+  week: "#f9731614",
+  old: "#dc262614",
+};
